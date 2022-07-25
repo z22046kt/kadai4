27abb79680f4c1c495dc6dadd7a7320df31942da
Kazuhiro Taguchi <z22046kt@aiit.ac.jp>
2022-07-25 09:16:08 +0900

    set travis CI setting files

---
+++ same-test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+tmp=/tmp/$$ # 変数を使って表記を短く
+
+# メッセージ定義
+echo "input 2 argments" > $tmp-args    # エラー：引数の数が正しくない
+echo "input natural number" > $tmp-nat # エラー：引数が数字以外を指定されている
+
+# 引数をエラーメッセージとしてエラー出力して、作成したtmpファイルを削除
+ERROR_EXIT () {
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1 エラー終了
+}
+
+# テスト開始
+# teat1: 引数の数が⾜りない
+./same.sh 2> $tmp-ans
+#diff $tmp-ans $tmp-args > /dev/null 2>&1
+#if [ $? -eq 0 ]; then
+#  ERROR_EXIT "error in test1"
+#fi
+
+diff $tmp-ans $tmp-args && ERROR_EXIT "error in test1"
---
+++ same.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then # 引数の数が2でない場合(not equal)
+  echo "input 2 argments" 1>&2 # エラーメッセージ
+  exit 1 # 終了ステータス
+fi
+
+expr $1 + $2 > /dev/null 2>&1 # exprに数字以外を渡すと終了ステータスが2以上になる
+
+if [ $? -ge 2 ]; then # その場合
+  echo "input natural number" 1>&2 # エラーメッセージ
+  exit 1 # 終了ステータス
+fi
+
+if [ $1 -eq $2 ]; then # ここから本編
+  echo "$1 is equal to $2" # ⼀致してる場合のメッセージ
+else
+  echo "$1 is not equal to $2" # ⼀致していない場合のメッセージ
+fi
